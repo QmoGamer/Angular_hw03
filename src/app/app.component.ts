cd5aa160303750da4ec208972ef208c5a703f407
Unknown <qhairmonster@gmail.com>
2017-09-21 14:04:41 +0800

    單元測試綠燈

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  checkboxText = 'I agree';
+  enableButton = false;
+
+  onChange(element: HTMLInputElement) {
+    this.enableButton = element.checked;
+  }
 }
